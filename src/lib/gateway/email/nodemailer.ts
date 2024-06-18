a72fbe3aa8f8d870bf08e0d023c7325974e1cf77
devitalo97 <dev.italo.souza@gmail.com>
2024-06-18 18:35:25 -0300

    feat: change auth method on nodemailer

--- src/lib/gateway/email/nodemailer.ts
+++ src/lib/gateway/email/nodemailer.ts
@@ -3,12 +3,14 @@ import nodemailer from 'nodemailer'
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    type: 'OAUTH2',
+    // type: 'OAUTH2',
+    // user: process.env.OAUTH_USER,
+    // clientId: process.env.OAUTH_CLIENT_ID,
+    // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    // refreshToken: process.env.OAUTH_REFRESH_TOKEN
     user: process.env.OAUTH_USER,
-    clientId: process.env.OAUTH_CLIENT_ID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    pass: process.env.GMAIL_PASS
   }
 });
-
+// https://dev.to/chandrapantachhetri/sending-emails-securely-using-node-js-nodemailer-smtp-gmail-and-oauth2-g3a
 export { transporter as mailGateway }
