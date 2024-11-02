b1fd4a5d3cbb8b8b41fb0336572999f750fd46da
devitalo97 <dev.italo.souza@gmail.com>
2024-11-02 17:13:11 -0300

    feat: add new page

--- src/components/ui/navbar/index.tsx
+++ src/components/ui/navbar/index.tsx
@@ -6,11 +6,20 @@ import { useRouter } from "next/router";
 import { titleByPathname } from "./data";
 import { useEffect, useState, useRef } from "react";
 import { useScroll, useTransform, motion, useAnimation } from "framer-motion";
-import path from "path";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog, DialogPanel } from "@headlessui/react";
 const inter = Inter({ subsets: ["latin"] });
+
+const navigation = [
+  { name: "Treinamentos", href: "/course" },
+  { name: "Galeria", href: "/gallery" },
+  { name: "Sobre", href: "/about" },
+  { name: "Entender para Atender", href: "/project/entender-para-atender" },
+];
 
 export default function Navbar() {
   const { pathname } = useRouter();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
   const { scrollY } = useScroll();
@@ -93,41 +102,99 @@ export default function Navbar() {
   }, [pathname]);
 
   return (
-    <S.Container
-      as={motion.div}
-      initial="visible"
-      animate={controls}
-      variants={{
-        visible: { top: "0px" },
-        hidden: { top: "-100px" },
-        color: {
-          background:
-            "linear-gradient(43deg, rgba(1,5,50,1) 0%, rgba(14,23,65,1) 100%)",
-        },
-        transparent: { background: "transparent" },
-      }}
-      inHome={pathname === "/"}
-      isScrolled={isScrolled}
-    >
-      <S.Wrapper>
-        <span>
-          <Logo />
-          <S.Title style={inter.style}>{titleByPathname[pathname]}</S.Title>
-        </span>
-        <S.Nav>
-          <Link href={"/about"}>
-            <S.Text style={inter.style}>Sobre</S.Text>
-          </Link>
-          {/* <Link href={"/services"}><S.Text style={inter.style}>Serviços</S.Text></Link> */}
-          <Link href={"/gallery"}>
-            <S.Text style={inter.style}>Galeria</S.Text>
-          </Link>
-          <Link href={"/course"}>
-            <S.Text style={inter.style}>Treinamentos</S.Text>
-          </Link>
-          {/* <Link href={"/blog"}><S.Text style={inter.style}>Blog</S.Text></Link> */}
-        </S.Nav>
-      </S.Wrapper>
-    </S.Container>
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav
+        aria-label="Global"
+        className="flex items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              alt=""
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              className="h-8 w-auto"
+            />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm/6 font-semibold text-gray-900"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              {/* <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div> */}
+              <span />
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
   );
 }
