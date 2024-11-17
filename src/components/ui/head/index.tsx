c74eed5825f35c59e0070aef00f660ff347035d1
devitalo97 <dev.italo.souza@gmail.com>
2024-11-17 20:07:12 -0300

    feat: change favicon.ico

--- src/components/ui/head/index.tsx
+++ src/components/ui/head/index.tsx
@@ -1,24 +1,19 @@
-
-import Head from 'next/head'
+import Head from "next/head";
 
 interface Props {
-    title: string,
-    content: string
+  title: string;
+  content: string;
 }
 
-export default function Header({
-    title,
-    content
-}: Props) {
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content={content} />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/logo_humane.ico" />
-            </Head>
-        </>
-
-    )
+export default function Header({ title, content }: Props) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={content} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo_humaning_color.ico" />
+      </Head>
+    </>
+  );
 }
